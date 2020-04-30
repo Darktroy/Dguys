a0086ed234aef5e644f71e67283d79e0346cfc72
mohammed osama <mohammedosama@ieee.org>
2020-04-30 09:36:30 +0200

    refactor LoginController

--- app/Http/Controllers/API/Auth/LoginController.php
+++ app/Http/Controllers/API/Auth/LoginController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers\API\Auth;
 
+use Illuminate\Support\Arr;
 use App\Http\Controllers\Controller;
+use App\Http\Resources\UserResource;
 use App\Http\Requests\Auth\LoginUserFormRequest;
 
 class LoginController extends Controller
@@ -12,14 +14,15 @@ class LoginController extends Controller
      */
     public function __invoke(LoginUserFormRequest $request)
     {
-        $credantial = ['password'=>$request->validated()['password']
-                ,'mobile'=>$request->validated()['mobile']
-                ,'type'=>$request->validated()['type']];
-//        if (auth()->attempt($request->validated())) {
-        if (auth()->attempt($credantial) ){
-           auth()->user()['device_id'] = $request->validated()['device_id'];
-           auth()->user()->save();
-            return response()->json(['token' => auth()->user()->generateAuthToken()]);
+        if (auth()->attempt(Arr::only($request->validated(), ['password', 'mobile', ' type']))) {
+            auth()->user()['device_id'] = $request->validated()['device_id'];
+            auth()->user()->save();
+
+            return (new UserResource(auth()->user()))->additional([
+                'meta' => [
+                    'token' => auth()->user()->generateAuthToken(),
+                ],
+            ]);
         }
 
         return response()->json(['error' => 'UnAuthorised'], 401);
